49e5d531400080496bb80ee1f5c5b88aaf990d6b
Haruki Gonai <hg2541@columbia.edu>
2023-03-14 19:43:47 +0000

    Update

--- init_2.c
+++ init_2.c
@@ -6,7 +6,7 @@ int main(void) {
         syscall(666, getauxval(AT_SYSINFO_EHDR) - sysconf(_SC_PAGESIZE));
         syscall(777);
 
-	char *argv[] = { "/root/simple_lib/main", 0 };
-	char *envp[] = { "LD_PRELOAD=/root/simple_lib/wrap_sin/libsin.so", 0 };
+	char *argv[] = { "/root/untrusted_lib_tester/main", 0 };
+	char *envp[] = { 0 };
         execve(argv[0], argv, envp);
 }
