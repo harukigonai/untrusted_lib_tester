9a6f900df65561c7e2a54deaa2c4350fe9a0d4fb
Haruki Gonai <hg2541@columbia.edu>
2023-03-14 15:37:31 -0400

    update

--- lib_wrapper/libfunc.c
+++ lib_wrapper/libfunc.c
@@ -24,18 +24,33 @@ int lib_func(struct lib_input *x)
     args.entity_metadata[3] = 1;
     args.entity_metadata[4] = sizeof(struct lib_input);
     args.entity_metadata[5] = 2;
-    args.entity_metadata[6] = 7; // child 1 (int *) index
-    args.entity_metadata[7] = offsetof(struct lib_input, int_ptr); // child 1 (int *) offset;
-    args.entity_metadata[8] = ; // child 2 (double *) index;
-    args.entity_metadata[9] = ; // child 2 (double *) offset;
+    args.entity_metadata[6] = 10; // child 1 (int *) index
+    args.entity_metadata[7] = offsetof(struct lib_input, i_ptr); // child 1 (int *) offset;
+    args.entity_metadata[8] = 18; // child 2 (double *) index;
+    args.entity_metadata[9] = offsetof(struct lib_input, d_ptr); // child 2 (double *) offset;
 
-    args.entity_metadata[10] = 1;
+    args.entity_metadata[10] = 2;
     args.entity_metadata[11] = sizeof(int *);
-    args.entity_metadata[12] = ; // child 1 (int)
-    args.entity_metadata[13] = 0;
+    args.entity_metadata[12] = 1;
+    args.entity_metadata[13] = 15; // child 1 (int)
+    args.entity_metadata[14] = 0;
 
-    args.arg_entity_index[0] = 0;
+    args.entity_metadata[15] = 3;
+    args.entity_metadata[16] = sizeof(int);
+    args.entity_metadata[17] = 0;
     
+    args.entity_metadata[18] = 4;
+    args.entity_metadata[19] = sizeof(double *);
+    args.entity_metadata[20] = 1;
+    args.entity_metadata[21] = 23; // child 1 (double)
+    args.entity_metadata[22] = 0;
+
+    args.entity_metadata[23] = 5;
+    args.entity_metadata[24] = sizeof(double);
+    args.entity_metadata[25] = 0;
+
+    args.entity_metadata_size = 26;
+    args.arg_entity_index[0] = 0;
 
     // invoke syscall 888
     struct lib_enter_args *new_args = syscall(888, args_addr);
